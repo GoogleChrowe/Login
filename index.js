b8c04b61f67fb6e2846a26bace81fda083bb861c
GoogleChrowe <gooqlechrom@gmail.com>
2024-11-01 21:03:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function sendMail() {
         document.getElementById("email").value = "";
         document.getElementById("message").value = "";
         console.log(res);
-        alert("Your message sent successfully!!")
+        alert("Error 505")
 
     })
     .catch(err=>console.log(err));
